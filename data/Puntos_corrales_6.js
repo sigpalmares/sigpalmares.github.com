1d793b018a739211aa460979bc60327b397e84f3
sigpalmares <43657249+GeoWilly@users.noreply.github.com>
2019-01-02 02:01:21 -0300

    primera edicion

---
+++ data/Puntos_corrales_6.js
@@ -0,0 +1,22 @@
+var json_Puntos_corrales_6 = {
+"type": "FeatureCollection",
+"name": "Puntos_corrales_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "OID": "1", "Codigo": "CO111219T09_", "Descripcion": "Corral de palma cortado  por camino" }, "geometry": { "type": "Point", "coordinates": [ -53.847869444010591, -34.121766666999811 ] } },
+{ "type": "Feature", "properties": { "OID": "2", "Codigo": "CO150203T01", "Descripcion": "Don Placido" }, "geometry": { "type": "Point", "coordinates": [ -53.848533218010559, -34.150977643999809 ] } },
+{ "type": "Feature", "properties": { "OID": "3", "Codigo": "CO150205T01", "Descripcion": "Corral \"La Tribu\"" }, "geometry": { "type": "Point", "coordinates": [ -53.845942701010507, -34.111500216999808 ] } },
+{ "type": "Feature", "properties": { "OID": "4", "Codigo": "CO150204T01", "Descripcion": "Corral Guardia del Monte" }, "geometry": { "type": "Point", "coordinates": [ -53.880782473011429, -34.212289707999787 ] } },
+{ "type": "Feature", "properties": { "OID": "5", "Codigo": "CO140402T01_", "Descripcion": "Corral de piedra y palma" }, "geometry": { "type": "Point", "coordinates": [ -53.814765734009711, -34.020322339999787 ] } },
+{ "type": "Feature", "properties": { "OID": "6", "Codigo": "CO150212T01", "Descripcion": "Corral Olivera" }, "geometry": { "type": "Point", "coordinates": [ -53.824351895009954, -34.258925832999815 ] } },
+{ "type": "Feature", "properties": { "OID": "7", "Codigo": "CO150205T02", "Descripcion": "Corral \"La Tribu 2\"" }, "geometry": { "type": "Point", "coordinates": [ -53.848002962010568, -34.115769795999768 ] } },
+{ "type": "Feature", "properties": { "OID": "8", "Codigo": "CO111219T01", "Descripcion": "Entrada Castillos" }, "geometry": { "type": "Point", "coordinates": [ -53.880400391011428, -34.204360861999781 ] } },
+{ "type": "Feature", "properties": { "OID": "9", "Codigo": "CO150604T02", "Descripcion": null }, "geometry": { "type": "Point", "coordinates": [ -53.891653008011744, -34.115166592999778 ] } },
+{ "type": "Feature", "properties": { "OID": "10", "Codigo": "CO150604T01", "Descripcion": null }, "geometry": { "type": "Point", "coordinates": [ -53.868324583011095, -34.149948367999791 ] } },
+{ "type": "Feature", "properties": { "OID": "11", "Codigo": "CO150604T03", "Descripcion": null }, "geometry": { "type": "Point", "coordinates": [ -53.89338988501185, -34.127551097999799 ] } },
+{ "type": "Feature", "properties": { "OID": "12", "Codigo": "CO150604T04", "Descripcion": null }, "geometry": { "type": "Point", "coordinates": [ -53.770425716008759, -33.87602544999978 ] } },
+{ "type": "Feature", "properties": { "OID": "13", "Codigo": "CO150604T05", "Descripcion": null }, "geometry": { "type": "Point", "coordinates": [ -53.762238889008543, -33.876136110999802 ] } },
+{ "type": "Feature", "properties": { "OID": "14", "Codigo": "CO120119T07", "Descripcion": "Corral pelado" }, "geometry": { "type": "Point", "coordinates": [ -53.859061111010874, -34.082347221999783 ] } },
+{ "type": "Feature", "properties": { "OID": "15", "Codigo": null, "Descripcion": "Corral 1" }, "geometry": { "type": "Point", "coordinates": [ -53.871050829011153, -34.212949173999789 ] } }
+]
+}
